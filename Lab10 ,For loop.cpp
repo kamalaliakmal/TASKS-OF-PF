426e6af54ab4b4ee84bfd327fdc60db8fe12cae6
Kamal Ali Akmal <kamalaliakmal@gmail.com>
2025-01-21 19:47:52 +0500

    Add files via upload
    These are the tasks of Programming Fundamentals from Lab 01 to Lab 14.

---
+++ Lab10 ,For loop.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  //  Write a program to print all numbers from 1 to N,where N is entered  by the user.
+  
+//	int i;
+//	int N;
+//	cout<<"Enter the number:";
+//	cin>>N;
+//	for(i=1;i<=N;i++)
+//	{
+//		 cout<<i<<" ";
+//	}
+	
+  // ***********************************************************
+	
+	// Write a program to display the multiplication table of a number entered by user.
+	
+//	int i,j,k,mult;
+//	cout<<"Enter the number: ";
+//	cin>>i;
+//    cout<<"Enter the ending number of multiplication table: ";
+//    cin>>k;
+//	for(j=1;j<=k;j++)
+//	{
+//        mult=i*j;
+//		cout<<i<<'*'<<j<<'='<<mult;
+//		cout<<endl;
+//	}
+	
+	
+  // ************************************************************
+  
+  // Write a C++ program to calculate the sum of the first N natural numbers(1,2,3.......N),where N is entered by the user.
+  
+//	int N,i;
+//	int sum=0;
+//	cout<<"Enter the number: ";
+//	cin>>N;
+//	for(i=1;i<=N;i++)
+//	{
+//		sum=sum+i;	
+//	}
+//	cout<<"Sum of first "<<N<<" natural number is = "<<sum;
+	
+  // *************************************************************
+  
+  /* write two programs that prints the following patterns.
+  
+  *  *  *  *                    *  *  *  *  *
+  *  *  *  *                    *  *  *  *
+  *  *  *  *                    *  *  *  
+  *  *  *  *                    *  *
+                                *
+   */                          
+                                
+//	int i,j;
+//	for(i=1;i<=4;i++)
+//	{
+//	  for(j=1;j<=4;j++)
+//	     {
+//	  	  cout<<"*"<<"  ";
+//		 }	
+//	  cout<<endl;	
+//	}
+	
+	
+	
+//	int i,j;
+//	for(i=5;i>=1;i--)
+//	{
+//		for(j=1;j<=i;j++)
+//		{
+//			cout<<"*"<<"  ";
+//		}
+//		cout<<endl;
+//		
+//	}
+
+  // **************************************************************
+   
+   /*
+   
+    Write a program that asks the user to enter a number and then calculates the factorial of the entered number.If the user enters a negative 
+   number, display a message stating that the factorial of negative numbers is not defined.
+   
+   */
+   
+  int i,j;
+  int fact=1;
+  cout<<"Enter the number : ";
+  cin>>i;
+  if(i<0)
+  {
+  	cout<<"The factorial of negative number is not defined.";
+  }                             
+	for(j=i;j>=1;j--)
+	{
+	   fact=fact*j;
+    }
+     cout<<"Factorial of "<<i<<" is = "<<fact;
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Lab8 task 1,2,3,4,5.cpp
@@ -0,0 +1,259 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+//	int a,b;
+//	char op;
+//	cout<<"Enter the first number:";
+//	cin>>a;
+//	cout<<"Enter the second number:";
+//	cin>>b;
+//	cout<<"Enter the operator:";
+//	cin>>op;
+//	switch(op)
+//	{
+//		case '+':
+//			cout<<"a+b="<<a+b;
+//			break;
+//		case '-':
+//			cout<<"a-b="<<a-b;
+//			break;
+//		case '*':	
+//		    cout<<"a*b="<<a*b;
+//			break;
+//		case '/':
+//		    cout<<"a/b="<<a/b;
+//			break;
+//		default:
+//		cout<<"You entered invalid operator.";	
+//	}
+	
+	
+//	int num;
+//	cout<<"Enter the number:";
+//	cin>>num;
+//	switch(num)
+//	{
+//		case 1:
+//			cout<<"Monday.";
+//			break;
+//		case 2:
+//		    cout<<"Tuesday.";
+//	        break;
+//		case 3:
+//		    cout<<"Wednesday.";
+//			break;
+//	    case 4:
+//		    cout<<"Thursday.";
+//			break;
+//		case 5:
+//		    cout<<"Friday.";
+//			break;
+//		case 6:
+//		    cout<<"Saturday.";
+//            break;
+//		case 7:
+//		    cout<<"Sunday.";
+//			break;
+//		default:
+//		    cout<<"You entered out of range number.";
+		
+//	}
+	
+//	const float PI=3.1415;
+//    int Area;
+//	int num;
+//	cout<<"Enter the number '1' for circle OR '2' for Rectangle OR '3' for Triangle :";
+//	cin>>num;
+//	switch(num)
+//	{
+//		case 1:
+//			int radius;
+//			cout<<"Enter the radius of circle:";
+//			cin>>r;
+//			Area=PI*(radius*radius);
+//			cout<<"Area of circle="<<Area;
+//			break;
+//		case 2:
+//			int length,width;
+//			cout<<"Enter the length of rectangle:";
+//			cin>>length;
+//			cout<<"Enter the width of rectangle:";
+//			cin>>width;
+//			Area=length*width;
+//			cout<<"Area of rectangle="<<Area;
+//			break;
+//		case 3:
+//			double base,height;
+//			cout<<"Enter the base of triangle:";
+//			cin>>base;
+//			cout<<"Enter the height of triangle:";
+//			cin>>height;
+//			Area=0.5*(base*height);
+//			cout<<"Area of triangle="<<Area;
+//			break;
+//		default:
+//			cout<<"You entered wrong numbrer.";	
+//	}
+	
+	
+//	cout<<"****************CURRENCY CONVERTER******************";
+//	int USD;
+//	cout<<"\nEnter the amount in USD:";
+//	cin>>USD;
+//	int num;
+//	cout<<"\nChoose  '1' for changing 'USD' into 'PKR'\n\t\t OR\n\t'2' for changing 'USD' into 'INR'\n\t\t OR\n\t'3' for changing 'USD' into 'Euros' :";
+//	cin>>num;
+//	switch(num)
+//	{
+//		case 1:
+//			int PKR;
+//			PKR=USD*280;
+//			cout<<"\n\tPakistani Rupees="<<PKR;
+//			break;
+//		case 2:
+//			int INR;
+//			INR=USD*84;
+//			cout<<"\n\tIndian Rupees="<<INR;
+//			break;
+//		case 3:
+//			double Euros;
+//			Euros=USD*0.95;
+//			cout<<"\n\tEuros="<<Euros;
+//			break;
+//	}
+	
+	
+//	int Electronics,Clothing,Groceries;
+    int quantity,total;
+	char category;
+	cout<<"Choose category of Item :";
+	cin>>category;
+	switch(category)
+	{
+		case 'E':
+			cout<<"You selected Electronic.";
+			int Electronics;
+			cout<<"\nChoose '1' for Laptop OR '2' for Smartphone OR '3' for Headphone:";
+			cin>>Electronics;
+			
+			switch(Electronics)
+			{
+				case 1:
+					cout<<"Laptop\nPrice of '1' Laptop=1000";
+					cout<<"\nEnter the number of quantity to purchase:";
+					cin>>quantity;
+					total=1000*quantity;
+					cout<<"\nTotal="<<total;
+					break;
+				case 2:
+					cout<<"Smartphone\nPrice of '1' Smartphone=700";
+					cout<<"\nEnter the number of quantity to purchase:";
+					cin>>quantity;
+					total=700*quantity;
+					cout<<"Total="<<total;
+					break;
+				case 3:
+					cout<<"Headphone\nPrice of '1' Headphone=150";
+					cout<<"\nEnter the number of quantity to purchase:";
+					cin>>quantity;
+					total=150*quantity;
+					cout<<"\nTotal="<<total;
+					
+					break;
+			}break;
+		case 'C':
+			cout<<"You selected Clothing.";
+			int Clothing;
+			cout<<"\nChoose '1' for Jacket OR '2' for T-shirt OR '3' for Jeans:";
+			cin>>Clothing;
+			int quantity,total;
+			switch(Clothing)
+			{
+				case 1:
+					cout<<"Jacket\nPrice of '1' Jacket=120";
+					cout<<"\nEnter the number of quantity to purchase:";
+					cin>>quantity;
+					total=120*quantity;
+					cout<<"\nTotal="<<total;
+					break;
+				case 2:
+					cout<<"T-shirt\nPrice of '1' T-shirt=40";
+					cout<<"\nEnter the number of quantity to purchase:";
+					cin>>quantity;
+					total=40*quantity;
+					cout<<"\nTotal="<<total;
+					break;
+				case 3:
+					cout<<"Jeans\nPrice of '1' Jeans=60";
+					cout<<"\nEnter the number of quantity to purchase:";
+					cin>>quantity;
+					total=60*quantity;
+					cout<<"\nTotal="<<total;
+					break;	
+					
+			}break;
+			
+			case 'G':
+	      	cout<<"You selected Groceries.";
+			int Groceries;
+			cout<<"\nChoose '1' for Milk OR '2' for Bread OR '3' for Egg:";
+			cin>>Groceries;
+			switch(Groceries)
+			{
+				case 1:
+					cout<<"Milk\n Price of '1' Milk=2";
+					cout<<"\nEnter the number of quantity to purchase:";
+					cin>>quantity;
+					total=150*quantity;
+					cout<<"\nTotal="<<total;
+					break;
+				case 2:
+					cout<<" Bread\n Price of '1'  Bread=3";
+					cout<<"\nEnter the number of quantity to purchase:";
+					cin>>quantity;
+					total=150*quantity;
+					cout<<"\nTotal="<<total;
+					break;
+				case 3:
+					cout<<"Egg\n Price of '1' Egg=5";
+					cout<<"\nEnter the number of quantity to purchase:";
+					cin>>quantity;
+					total=150*quantity;
+					cout<<"\nTotal="<<total;
+					break;
+           	}break;
+			
+	}
+	int discount;
+	if(total>500)
+	{
+	
+	discount=(20*total)/100;
+	cout<<"\nDiscount="<<discount;
+	}
+	else if(total>=100 && total<=500)
+	{
+	discount=(10*total)/100;
+	cout<<"\nDiscount="<<discount;
+    }
+    else
+	{
+		cout<<"\nNo Discount";
+	}
+	
+	
+	cout<<"\n"<<setw(50)<<setfill('*')<<"*"<<endl;
+	cout<<"\t\t\t\t----------------------------------\n";
+	cout<<setw(25)<<setfill('$')<<"$";
+	cout<<"\tCHALLAN    OF    BYING    PRODUCTS\t";
+	cout<<setw(25)<<setfill('$')<<"$"<<endl;
+	cout<<setw(25)<<"-----------------------------------------------------------------------------------------------"<<endl;
+	cout<<"Category\t\t Quantity\t\t Total\t\t Discount "<<endl;
+	cout<<category<<"\t\t\t "<<quantity<<"\t\t\t "<<total<<"\t\t "<<discount<<endl;
+	cout<<setw(25)<<"-----------------------------------------------------------------------------------------------"<<endl;
+	
+	
+	return 0;
+}
